3fef3be13b88d22d50ace51008be8650ecc323d3
Sergey Kiyanitsyn <k.sergey@wcode.club>
2026-08-14 01:05:01 +0300

    upd checking_date and checkout_date

--- conftest.py
+++ conftest.py
@@ -20,8 +20,8 @@ def booking_dates():
     checkout_date = checking_date + datetime.timedelta(days=5)
 
     return {
-        "checking_date": checking_date.strftime("%Y-%m-%d"),
-        "checkout_date": checkout_date.strftime("%Y-%m-%d"),
+        "checkin": checking_date.strftime("%Y-%m-%d"),
+        "checkout": checkout_date.strftime("%Y-%m-%d"),
     }
 
 
@@ -32,7 +32,7 @@ def generate_random_booking_data(booking_dates):
     last_name = faker.last_name()
     total_price = faker.random_number(digits=3)
     deposit_paid = faker.boolean()
-    additional_needs = faker.sentence()
+    additional_needs = faker.word()
 
     data = {
         "firstname": first_name,
