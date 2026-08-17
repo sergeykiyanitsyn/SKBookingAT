926b1e95440c9277bb2cdba429c84bcd54fa2b92
Sergey Kiyanitsyn <k.sergey@wcode.club>
2026-08-17 23:52:28 +0300

    add tests on create_booking

--- tests/test_booking_create.py
+++ tests/test_booking_create.py
@@ -1,12 +1,14 @@
 import allure
 import jsonschema
 import pytest
+from jsonschema import ValidationError
 from requests.exceptions import HTTPError
 
+from core.models.booking import BookingRenounce
 from core.shemas.create_booking_schema import CREATE_BOOKING_RESPONSE_SCHEMA
 
 
-@allure.feature("Tests Ping")
+@allure.feature("Tests create booking")
 class TestBooking:
 
     @allure.suite("Happy path: success create booking")
@@ -133,3 +135,73 @@ class TestBooking:
 
         with allure.step('Name match check'):
             response_json["firstname"] = booking_data["firstname"]
+
+    @allure.suite("Positive test: create booking with random date")
+    def test_create_booking_with_random_data(self, api_client, generate_random_booking_data):
+
+        with allure.step('Data preparation'):
+            booking_data = generate_random_booking_data.copy()
+
+        with allure.step('Send request'):
+            response = api_client.create_booking(booking_data)
+            response_json = response.json()
+
+        with allure.step('Validate response schema'):
+            try:
+                BookingRenounce(**response_json)
+            except ValidationError as e:
+                raise ValidationError(f'Response validation error: {e}')
+
+        with allure.step('Comparison of sent and received data'):
+            assert response_json[
+                       'booking'] == generate_random_booking_data, f"Expected booking data is {generate_random_booking_data}, got {response['bookingid']}"
+
+    @allure.suite("Positive test: create booking without additionalneeds")
+    def test_create_booking_without_additionalneeds(self, api_client, generate_random_booking_data):
+        with allure.step('Data preparation'):
+            booking_data = generate_random_booking_data.copy()
+            booking_data.pop("additionalneeds")
+
+        with allure.step('Send request'):
+            response = api_client.create_booking(booking_data)
+            response_json = response.json()
+
+        with allure.step('Validate response schema'):
+            try:
+                BookingRenounce(**response_json)
+            except ValidationError as e:
+                raise ValidationError(f'Response validation error: {e}')
+
+    @allure.suite("Positive test: create booking with correct depositpaid")
+    @pytest.mark.parametrize(
+        "depositpaid",
+        [True, False],
+    )
+    def test_create_booking_with_correct_depositpaid_data(self, api_client, generate_random_booking_data, depositpaid):
+        with allure.step('Data preparation'):
+            booking_data = generate_random_booking_data.copy()
+            booking_data["depositpaid"] = depositpaid
+
+        with allure.step('Send request'):
+            response = api_client.create_booking(booking_data)
+            response_json = response.json()
+
+        with allure.step('Check depositpaid'):
+            assert response_json[
+                       'booking']["depositpaid"] == booking_data["depositpaid"]
+    
+    @allure.suite("Positive test: create booking with correct depositpaid")
+    def test_create_booking_with_invalid_depositpaid_data(self, api_client, generate_random_booking_data):
+        with allure.step('Data preparation'):
+            booking_data = generate_random_booking_data.copy()
+            booking_data["depositpaid"] = None
+
+        with allure.step('Send request'):
+            with pytest.raises(HTTPError) as exc_info:
+                api_client.create_booking(booking_data)
+
+            response = exc_info.value.response
+
+        with allure.step('Assert status code is 500'):
+            assert response.status_code == 500, f'Expected status code 500, but got {response.status_code}'
+            assert response.text == 'Internal Server Error'
